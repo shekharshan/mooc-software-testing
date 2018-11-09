eee0b7ffd75392ebef2dd4526a6ff866648ee2ec
Shantanu Shekhar (Contractor) <s_shekhar@acs.org>
2018-11-09 10:21:44 -0500

    Bug for Practical exercise 1

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,19 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    private GHappy happy;
+
+    @BeforeEach
+    public void initialize() {
+       happy = new GHappy();
+    }
+    @Test
+    public void testEmptyString() {
+        Assertions.assertFalse(happy.gHappy("g"));
+    }
 }
